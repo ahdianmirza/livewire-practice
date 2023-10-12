d5d557d6ae6319b04c524838f8c4ea794dc96271
ahdianmirza <ahdianmirza@gmail.com>
2023-10-12 20:45:42 +0700

    Validation

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -4,14 +4,22 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
 
 class Clicker extends Component
 {
-    public $name;
-    public $email;
-    public $password;
+    #[Rule('required|min:2|max:255')]
+    public $name = "";
+
+    #[Rule('required|email|unique:users')]
+    public $email = "";
+
+    #[Rule('required|min:5')]
+    public $password = "";
 
     public function createNewUser() {
+        $this->validate();
+
         User::create([
             'name' => $this->name,
             "email" => $this->email,
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,10 +1,23 @@
 <div>
-    <form wire:submit="createNewUser" action="">
-        <input wire:model="name" type="text" placeholder="name">
-        <input wire:model="email" type="email" placeholder="email">
-        <input wire:model="password" type="password" placeholder="password">
+    <form class="p-5" wire:submit="createNewUser" action="">
+        <input class="block rounded border border-gray-100 px-3 py-1" wire:model="name" type="text" placeholder="name">
+        @error('name')
+            <span class="text-red-500 text-xs">{{ $message }}</span>
+        @enderror
 
-        <button>Create</button>
+        <input class="block rounded border border-gray-100 px-3 py-1 mt-1" wire:model="email" type="email"
+            placeholder="email">
+        @error('email')
+            <span class="text-red-500 text-xs">{{ $message }}</span>
+        @enderror
+
+        <input class="block rounded border border-gray-100 px-3 py-1 mt-1" wire:model="password" type="password"
+            placeholder="password">
+        @error('password')
+            <span class="text-red-500 text-xs">{{ $message }}</span>
+        @enderror
+
+        <button class="block rounded px-3 py-1 bg-gray-400 text-white mt-1">Create</button>
     </form>
 
     <hr>
